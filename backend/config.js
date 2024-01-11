f3f99c4af6ea89c97335380b3713570c0b8d485d
Anubhab Debnath <anubhabdebnath98@gmail.com>
2024-01-12 02:46:17 +0530

    Update config.js with new MongoDB credentials

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const MONGO_URL =
-  "mongodb+srv://anubhab:Anubhab_2004@cluster1.mls2riq.mongodb.net/books-collection?retryWrites=true&w=majority";
+  "mongodb+srv://anubhab:LDeJ83riIqdsqO1c@cluster1.mls2riq.mongodb.net/books-collection?retryWrites=true&w=majority";
